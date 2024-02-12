05173c04ffc5814a4dd5f5a22046edfe7371d221
Bony diez <Bonypablo@gmail.com>
2024-02-12 09:46:03 +0100

    Primer commit Rama 1

--- PruebasGit/src/PruebasGit/PruebasGit.java
+++ PruebasGit/src/PruebasGit/PruebasGit.java
@@ -5,6 +5,7 @@ public class PruebasGit {
 	public static void main(String[] args) {
 		System.out.println(" PRUEBAS E GIT");
 		System.out.println(" Segundo commit master ");
+		System.out.println("Primer commit Rama1");
 	}
 
 }
