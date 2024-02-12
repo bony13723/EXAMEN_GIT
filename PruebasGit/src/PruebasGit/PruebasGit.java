6e59488818fa5d5babefe5fe7ebbc9bf46c9f315
Bony diez <Bonypablo@gmail.com>
2024-02-12 10:40:31 +0100

    Tercer commit Master

--- PruebasGit/src/PruebasGit/PruebasGit.java
+++ PruebasGit/src/PruebasGit/PruebasGit.java
@@ -3,9 +3,10 @@ package PruebasGit;
 public class PruebasGit {
 
 	public static void main(String[] args) {
-		System.out.println(" PRUEBAS E GIT");
+		System.out.println(" PRUEBAS E GIT ");
 		System.out.println(" Segundo commit master ");
-		System.out.println("Primer commit Rama1");
+		System.out.println(" Primer commit Rama1 ");
+		System.out.println(" Tercer commit master ");
 	}
 
 }
